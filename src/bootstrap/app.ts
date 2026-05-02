d5ddcd73e079cda12faf9daeddfea5e2eef24668
romeucajamba <romeucajamba@gmail.com>
2026-05-02 14:00:39 +0100

    remoção de cookies

--- src/bootstrap/app.ts
+++ src/bootstrap/app.ts
@@ -1,29 +1,12 @@
 import express from 'express';
 import RateLimitServices from '@/infra/services/ratelimite/index';
-import CookieService from '@/infra/services/auth/cookie';
-import session from 'express-session';
-import cookieParser from 'cookie-parser';
-import { env } from '@/shared/env/env';
 import { router } from '@/http/routes';
 import { errorHandler } from '@/http/middleware/errorHandler';
 
 export const appExpress = express();
 
 appExpress.use(RateLimitServices.apply());
-appExpress.use(CookieService.parser());
 
-appExpress.use(session({
-  secret: env.JWT_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: env.ENVOIRONMENT === 'production',
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000 
-  }
-}));
-
-appExpress.use(cookieParser());
 appExpress.use(express.json());
 
 // 1. Injetar Rotas Principais
--- src/utils/enums.ts
+++
@@ -1,7 +0,0 @@
-export const ECOOKIES = {
-  COOKIE_USER_AUTH_TOKEN: "meta-tk",
-  HEADER_API_AUTH_TOKEN: "meta-tk",
-  HEADER_API_SERVER_TOKEN: "meta-yt",
-  HEADER_SERVER_BY_SERVER_TOKEN: "rsa-s-by-s-token",
-  LOCAL_SERVER_TOKEN: ""
-};
