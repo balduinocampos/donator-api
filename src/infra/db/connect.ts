8c9da85b59e61d6046fdd48fbb55c8d60a4bf032
romeucajamba <romeucajamba@gmail.com>
2026-05-02 12:45:25 +0100

    resolvido o generated

--- src/infra/db/connect.ts
+++ src/infra/db/connect.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '../../../prisma/generated/prisma';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
